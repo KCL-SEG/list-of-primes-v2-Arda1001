acad714c85c96efd41ac2ba5c571125fcf776943
Arda Ordu <ardaordu03@gmail.com>
2022-09-28 23:21:12 +0100

    Complete prime numbers exercise v2

--- primes.py
+++ primes.py
@@ -3,4 +3,20 @@
 
 def primes(number_of_primes):
     list = []
+    count = 0
+    number = 2
+    
+    if number_of_primes <= 0:
+        raise ValueError("number_of_primes must be greater than 0")
+
+    while count < number_of_primes:
+         prime = True
+         for i in range(2, number):
+             if number % i == 0:
+                 prime = False
+                 break
+         if prime:
+             list.append(number)
+             count += 1
+         number += 1
     return list
